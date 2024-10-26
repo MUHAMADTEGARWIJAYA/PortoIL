878a1ef677043dcd2baaa046f7721563504a39d5
unknown <tegarmuhamad922@gmail.com>
2024-10-26 20:26:36 +0700

    mengubah warga text h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      WOIIIII
-  </h1>
+    <h1 className="text-3xl font-bold underline" color="red">WOIIIII</h1>
+    
   )
 }
 
